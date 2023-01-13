54f81a493bac4cf544891544136d774c6dd409e4
silvia.orenes <silvia.orenes@u-tad.com>
2023-01-13 10:05:40 +0100

    creado constructor de Persona

--- PROYECTOVERSIONES_A/src/rrhh/Persona.java
+++ PROYECTOVERSIONES_A/src/rrhh/Persona.java
@@ -1,5 +1,17 @@
 package rrhh;
 
 public class Persona {
-
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private int edad;
+	
+	public Persona(String nombre, String apellidos, String dni, int edad) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.edad = edad;
+	}
+	
+	
 }
