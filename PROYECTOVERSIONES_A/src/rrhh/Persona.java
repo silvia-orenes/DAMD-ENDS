c43e64a7d6a8e05c94f11f9530e33dc485a226dc
silvia.orenes <silvia.orenes@u-tad.com>
2023-01-13 10:06:17 +0100

    Creados los metodos setter y getter

--- PROYECTOVERSIONES_A/src/rrhh/Persona.java
+++ PROYECTOVERSIONES_A/src/rrhh/Persona.java
@@ -12,6 +12,40 @@ public class Persona {
 		this.dni = dni;
 		this.edad = edad;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 	
 	
 }
