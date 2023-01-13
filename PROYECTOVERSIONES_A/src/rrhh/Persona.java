36cc2532a12b24258644d4b7f2e75f6cca06dd18
silvia.orenes <silvia.orenes@u-tad.com>
2023-01-13 10:40:41 +0100

    Creacion clase principal y método getEstacion

--- PROYECTOVERSIONES_A/src/rrhh/Persona.java
+++ PROYECTOVERSIONES_A/src/rrhh/Persona.java
@@ -1,51 +1,98 @@
 package rrhh;
 
 public class Persona {
-	private String nombre;
-	private String apellidos;
-	private String dni;
-	private int edad;
+	//declaramos los atributos como privados
+		private String nombre;
+		private String apellidos;
+		private String fecha_cumple; //la declaro como String porque no hemos visto aún los datos de tipo Date
+		private int edad;
+		private String sexo;
+		
+		//constructor
+		public Persona(String nombre, String apellidos, String fecha_cumple, int edad, String sexo) {
+			this.nombre = nombre;
+			this.apellidos = apellidos;
+			this.fecha_cumple = fecha_cumple;
+			this.edad = edad;
+			this.sexo = sexo;
+		}
+
+		//métodos getter y setter
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getApellidos() {
+			return apellidos;
+		}
+
+		public void setApellidos(String apellidos) {
+			this.apellidos = apellidos;
+		}
+
+		public String getFecha_cumple() {
+			return fecha_cumple;
+		}
+
+		public void setFecha_cumple(String fecha_cumple) {
+			this.fecha_cumple = fecha_cumple;
+		}
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		public String getSexo() {
+			return sexo;
+		}
+
+		public void setSexo(String sexo) {
+			this.sexo = sexo;
+		} 
 	
-	public Persona(String nombre, String apellidos, String dni, int edad) {
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.dni = dni;
-		this.edad = edad;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellidos() {
-		return apellidos;
-	}
-
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
+	/* método que dada la fecha de nacimiento, nos diga si la persona 
+	 * ha nacido en primavera, verano, otoño o invierno
+	 */
 	
-	
+	public void getEstacionNacimiento(String fecha) {
+		
+		String estacion;
+		String dia = fecha.substring(0,2); //extraigo la subcadena que hay desde la posición 0 a la 2
+		
+		//extraigo el mes de la variable fecha que debe tener formato dd/mm/yyyy.
+		//indicándole que coja la subcadena que empieza en la posición siguiente a la primera /
+		//y que termina dos posiciones despues
+		String mes = fecha.substring(fecha.indexOf("/")+1,fecha.indexOf("/")+3);
+		
+		//transformamos dia y mes a enteros para poder hacer comparaciones con > e ==
+		int diaNac = Integer.parseInt(dia);
+		int mesNac = Integer.parseInt(mes);
+		
+		//Primavera: inicia el 20 de marzo al 21 de junio. 
+		if((diaNac>=20 && mesNac==3)|| mesNac==4 || mesNac==5 || (diaNac<=21 && mesNac==6))
+			estacion = "primavera";
+		
+		//Verano: inicia el 21 de junio y finaliza el 23 de septiembre. 
+		else if ((diaNac>=21 && mesNac==6)|| mesNac==7 || mesNac==8 || (diaNac<=23 && mesNac==9))
+			estacion = "verano";
+		
+		//Otoño: inicia el 23 de septiembre y finaliza el 21 de diciembre.
+		else if ((diaNac>=23 && mesNac==9)|| mesNac==10 || mesNac==11 || (diaNac<=21 && mesNac==12))
+			estacion = "otoño";
+		else
+			estacion = "invierno";
+		//Invierno: inicia el 21 de diciembre y finaliza el 20 de marzo.	 
+		
+		System.out.println(this.nombre + " has nacido en " + estacion);
+	}
 	
 	
 }
---
+++ PROYECTOVERSIONES_A/src/rrhh/PrincipalPersona.java
@@ -0,0 +1,53 @@
+package rrhh;
+
+import java.util.Scanner;
+
+public class PrincipalPersona {
+	
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String nombre, apellidos, fecha_cumple, sexo; 
+		int edad;
+		
+		System.out.println("Introduce el nombre de la persona 1: ");
+		nombre = sc.nextLine();
+		System.out.println("Introduce los apellidos de la persona 1: ");
+		apellidos = sc.nextLine();
+		System.out.println("Introduce la fecha de nacimiento de la persona 1 con formato dd/mm/yyyy: ");
+		fecha_cumple = sc.nextLine();
+		System.out.println("Introduce el sexo de la persona 1: ");
+		sexo = sc.nextLine();
+		System.out.println("Introduce la edad de la persona 1: ");
+		edad = sc.nextInt();
+		sc.nextLine();
+			
+		//creo el objeto Persona con los datos
+		Persona p1 = new Persona(nombre, apellidos,fecha_cumple,edad,sexo);
+		//muestro la persona creada por consola
+		p1.toString();
+		
+		
+		//PEDIMOS LOS DATOS DE LA SEGUNDA PERSONA
+		System.out.println("Introduce el nombre de la persona 2: ");
+		nombre = sc.nextLine();
+		System.out.println("Introduce los apellidos de la persona 2: ");
+		apellidos = sc.nextLine();
+		System.out.println("Introduce la fecha de nacimiento de la persona 2 con formato dd/mm/yyyy: ");
+		fecha_cumple = sc.nextLine();
+		System.out.println("Introduce el sexo de la persona 2: ");
+		sexo = sc.nextLine();
+		System.out.println("Introduce la edad de la persona 2: ");
+		edad = sc.nextInt();
+		sc.nextLine();
+			
+		//creo el objeto Persona con los datos
+		Persona p2 = new Persona(nombre, apellidos,fecha_cumple,edad,sexo);
+		//muestro la persona creada por consola
+		p2.toString();
+		
+		p1.getEstacionNacimiento(p1.getFecha_cumple());
+		p2.getEstacionNacimiento(p2.getFecha_cumple());
+		
+	}
+}
